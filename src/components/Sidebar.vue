dcf18970c21c51f6646eeff9d44db8257a1260dc
Pär Thernström <par.thernstrom@gmail.com>
2017-01-05 22:07:06 +0100

    make sidebar wider

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,8 +1,27 @@
+<style>
+  .mdl-layout__drawer {
+    width: 340px;
+    transform: translateX(-350px)
+  }
+  .mdl-layout--fixed-drawer>.mdl-layout__content {
+    margin-left: 340px;
+  }
+  .mdl-layout--fixed-drawer.is-upgraded:not(.is-small-screen)>.mdl-layout__header {
+      margin-left: 340px;
+      width: calc(100% - 340px);
+  }
+
+  @media screen and (max-width: 1024px) {
+    .mdl-layout--fixed-drawer>.mdl-layout__content {
+      margin-left: 0;
+    }
+  }
+  </style>
 <template>
 
   <div class="mdl-layout__drawer">
 
-    <span class="mdl-layout-title">{{title}}</span>
+    <!-- <span class="mdl-layout-title">{{title}}</span> -->
 
     <div v-mdl class="mdl-textfield mdl-js-textfield">
       <input v-mdl class="mdl-textfield__input" type="text" id="sample1">
