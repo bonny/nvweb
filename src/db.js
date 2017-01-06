372af52ea58a8185dc5a856164912d3dcff7b03f
Pär Thernström <par.thernstrom@gmail.com>
2017-01-06 21:50:36 +0100

    add note using faker

--- src/db.js
+++ src/db.js
@@ -1,5 +1,6 @@
 
 import Dexie from 'dexie'
+let faker = require('faker')
 
 let db = new Dexie('MyDatabase')
 
@@ -9,30 +10,24 @@ db.version(1).stores({
 })
 
 // Init DB with some default statuses:
-db.on('populate', function () {
-  for (let i = 0; i < 5; i++) {
-    let notesToAdd = [
-      {
-        name: 'Note name ' + Math.round(Math.random() * 999),
-        dateModified: Date.now(),
-        previewText: 'Lorem ipsum dolor sit amet',
-        text: 'Lorem ipsum dolor sit amet :) Några smileys: ✌️📝🗒 consectetur adipiscing elit.\nNon quaeritur autem quid naturae tuae consentaneum sit, sed quid disciplinae. Deinde disputat, quod cuiusque generis animantium statui deceat extremum.'
-      },
-      {
-        name: 'En till 🗒 yo ' + Math.round(Math.random() * 999),
-        dateModified: Date.now(),
-        previewText: 'Look, just because I don\'t be givin\' no man a foot massage don\'t make it right',
-        text: 'Meheh 👍🏽\naturae tuae consentaneum sit, sed quid disciplinae. Deinde disputat, quod cuiusque generis animantium statui deceat extremum.',
-        dropboxData: {
-          hej: 'hopp',
-          yolo: 'hashtag'
-        }
-      }
-    ]
-
-    db.notes.bulkPut(notesToAdd)
+// db.on('populate', function () {
+  // for (let i = 0; i < 5; i++) {
+let notesToAdd = [
+  {
+    name: faker.lorem.sentences(2),
+    previewText: faker.lorem.sentences(3),
+    text: faker.lorem.paragraphs(10),
+    dateModified: Date.now(),
+    dropboxData: {
+      hej: 'hopp',
+      yolo: 'hashtag'
+    }
   }
-})
+]
+
+db.notes.bulkPut(notesToAdd)
+  // }
+// })
 
 /*
 var res2 = db.options.add({
