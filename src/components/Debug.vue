03fad73b0882b70d653fba195dbe30ae4328e7b0
Pär Thernström <par.thernstrom@gmail.com>
2017-01-11 19:54:17 +0100

    remove comment

--- src/components/Debug.vue
+++ src/components/Debug.vue
@@ -30,53 +30,7 @@ import NotesList from '../components/NotesList.vue'
 export default {
   name: 'Debug',
   mounted () {
-
-    /*
-    db.options.where('key').equals('dropboxNotesFolder').first().then((val) => {
-
-      this.dropboxNotesFolder = val && val.value ? val.value : null
-
-      if (!this.dropboxNotesFolder) {
-        return
-      }
-
-      var dbx = this.DropboxStorage.getFolderList(this.dropboxNotesFolder)
-        .then((response) => {
-          //console.log(response)
-
-          // keep .txt and .md
-          var validExtensionsRegEx = new RegExp(/\.(txt|md)$/)
-          var entries = response.entries
-          entries = response.entries.filter((val) => {
-
-            // must be file
-            if (val['.tag'] != 'file') {
-              return false;
-            }
-
-            return validExtensionsRegEx.test(val.name)
-
-          });
-
-          // order by server modification date
-          entries = entries.sort((a, b) => {
-            if (a.server_modified > b.server_modified) return -1
-            if (a.server_modified < b.server_modified) return 1
-            return 0
-          });
-
-          console.log(entries)
-          this.NotesFolderFiles = entries
-        })
-        .catch(function(error) {
-          console.error(error)
-        });
-
-    });
-    */
-
   },
-
   computed: {
     options () {
       return this.$store.state.options
@@ -85,11 +39,9 @@ export default {
       return this.$store.state.notes
     }
   },
-
   components: {
     NotesList
   }
-
 }
 
 </script>
