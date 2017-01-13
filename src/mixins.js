93e8147a8c2789f0733a06dfdd8b5e60be915271
Pär Thernström <par.thernstrom@gmail.com>
2017-01-13 19:57:39 +0100

    make sure new note name is unique

--- src/mixins.js
+++ src/mixins.js
@@ -22,34 +22,49 @@ var mixins = {
       }
 
       // Check if note with same title exists
-      /*
-      db.notes.where('name').equals(title).toArray().then((notesWithSameName) => {
-        // console.log('vals', vals)
-        return notesWithSameName
-      }).catch((err) => {
-      */
+      // Get all notes beginning with same title, then loop until unique found
+      db.notes.where('name').startsWith(newNote.name).toArray().then((notesWithSameBeginning) => {
+        // while name not unique keep testing
+        let loop = 0
+        let newNameSuggestion = newNote.name
+        let foundSameName = true
 
-      db.notes.put(newNote).then((newNoteID) => {
-        // Add new note to notes list
-        console.log('note added', newNote)
+        while (foundSameName) {
+          foundSameName = notesWithSameBeginning.find((note) => {
+            return note.name === newNameSuggestion
+          })
 
-        this.$store.state.notes.unshift(newNote)
+          if (foundSameName) {
+            loop++
+            newNameSuggestion = `${newNote.name} ${loop}`
+          }
+        }
 
-        // Edit and focus new note
-        this.$root.$emit('NoteSelectedInNotesListGoEdit', newNote.id)
+        newNote.name = newNameSuggestion
 
         return
+      }).then(() => {
+        db.notes.put(newNote).then((newNoteID) => {
+          // Add new note to notes list
+          console.log('note added', newNote)
+
+          this.$store.state.notes.unshift(newNote)
+
+          // Edit and focus new note
+          this.$root.$emit('NoteSelectedInNotesListGoEdit', newNote.id)
+
+          return
+        })
       })
     },
     /**
      * Close drawer/sidebar if it is opneed
      */
     closeDrawerIfOpened () {
-
       let layout = document.querySelector('.mdl-layout')
       let drawer = document.querySelector('.mdl-layout__drawer')
 
-      if (layout.classList.contains("is-small-screen") && drawer.classList.contains("is-visible") ) {
+      if (layout.classList.contains('is-small-screen') && drawer.classList.contains('is-visible')) {
         layout.MaterialLayout.toggleDrawer()
       }
     },
@@ -57,11 +72,10 @@ var mixins = {
      * Open drawer/sidebar if it is closed
      */
     openDrawerIfClosed () {
-
       let layout = document.querySelector('.mdl-layout')
       let drawer = document.querySelector('.mdl-layout__drawer')
 
-      if (layout.classList.contains("is-small-screen") && ! drawer.classList.contains("is-visible") ) {
+      if (layout.classList.contains('is-small-screen') && !drawer.classList.contains('is-visible')) {
         layout.MaterialLayout.toggleDrawer()
       }
     }
